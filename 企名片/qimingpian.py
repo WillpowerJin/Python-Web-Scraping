aa13c4c6d652c13f9223f1611dd698683ad06bb4
WillpowerJin <1066234044@qq.com>
2023-09-15 11:27:59 +0800

    update

--- 企名片/qimingpian.py
+++ 企名片/qimingpian.py
@@ -1,5 +1,7 @@
-import requests
+# Destination URL: https://www.qimingpian.com/finosda/project/pinvestment
+
 import sys
+import requests
 import execjs
 import json
 
@@ -30,15 +32,15 @@ proxies = {
     "https": "127.0.0.1:7890"
 }
 
-response = session.post(url=url, proxies=proxies).json()
+response = session.post(url=url, data=data, proxies=proxies).json()
 encrypt_data = response["encrypt_data"]
 # print(encrypt_data)
 
+# execjs 调用 JS 代码
 with open(f"{folder_path}/qimingpian.js", "r", encoding="utf-8") as f:
     js_code = f.read()
+data = execjs.compile(js_code).call("s", encrypt_data)
 
-result = execjs.compile(js_code).call("s", encrypt_data)
-# print(result)
-
+# 保存 json 文件
 with open(f"{folder_path}/qimingpian_data.json", "w", encoding="utf-8") as f:
-    json.dump(result, f, ensure_ascii=False)
+    json.dump(data, f, ensure_ascii=False)
